0093bc1ff48bc489a7a451e30162311ded811610
markupet <markupet@csc.kth.se>
2014-01-06 16:20:11 +0100

    Added environment files

---
+++ src/Environment.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include<vector>
+#include<string>
+
+#include"Direction.h"
+//#include"Actor.h"
+#include"Object.h"
+ 
+namespace licf
+{
+	class Actor;
+	
+	class Environment
+	{
+	public:
+		virtual std::vector<Environment*> directions() = 0;
+		virtual Environment * neighbor(Direction) = 0;
+		virtual std::string description() = 0;
+		virtual void enter(Actor &) = 0;
+		virtual void leave(Actor &) = 0;
+		virtual bool pick_up(Object *) = 0;
+		virtual bool drop(Object *) = 0;
+	};
+}
