ea9ef79d71b5a46b7f97fd5f75e4ddafc3f53a24
linuseh <leh_87@hotmail.com>
2014-01-10 11:30:26 +0100

    added key

---
+++ src/objects/Key.cpp
@@ -0,0 +1,11 @@
+#include "key.h"
+
+namespace licf {
+	std::string Key::description() const {
+		std::stringstream ss;
+		ss << type() << ":\n";
+		ss << "Vikt: " << weight() << '\n';
+		ss << "Volym: " << volume() << '\n';
+		return ss.str();
+	}
+}
---
+++ src/objects/Key.h
@@ -0,0 +1,13 @@
+#include "Object.h"
+
+namespace licf {
+	class Key : public Object {
+	public:
+		std::string description() const;
+
+        double weight() const { return 2; }
+		double volume() const { return 1; }
+
+		std::string type() const { return "key"; }
+	};
+}
